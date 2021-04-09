e9a421f336a6331f992264efe5c821ed00f30b9f
josiahjon <josiahtalebi@gmail.com>
2021-04-08 21:50:11 -0700

    update

--- project3/main.cpp
+++ project3/main.cpp
@@ -2,20 +2,54 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct TAG_NODE {
-    int v;          //vertex
-    float w;        //weight
-    TAG_NODE* next;
-}NODE;
-typedef NODE* pNODE;
+struct AdjListNode{
+    int dest;
+    int weight;
+    struct AdjListNode* next;
+};
 
-void addEdge(pNODE* A, int u, int v) {
-    pNODE->v = u;
-   
+struct AdjList{
+    struct AdjListNode* head;
+};
+
+struct Graph{
+    int V;
+    struct AdjList* array;
+};
+struct AdjListNode* newAdjListNode(int dest){
+    struct AdjListNode* newNode = (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->next = NULL;
+    return newNode;
 }
 
-void readFromFile(std::string fileName) {
+
+struct Graph* createGraph(int vertices){
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->V = vertices; 
+    graph->array = (struct AdjList*) malloc(vertices * sizeof(struct AdjList));
+    int i;
+    for (i = 0; i < vertices; ++i)
+        graph->array[i].head = NULL;
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest, int weight){
+    struct AdjListNode* newNode = newAdjListNode(dest);
+    newNode->next = graph->array[src].head;
+    newNode->weight = weight;
+    graph->array[src].head = newNode;
+    
+    //if undirected, same back
+    newNode = newAdjListNode(src);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+
+void readFromFile(std::string fileName){
     int vertices;
     int edges;
     int edgeID;
@@ -27,25 +61,22 @@ void readFromFile(std::string fileName) {
     if (inFile.is_open()) {
         inFile >> vertices;
         inFile >> edges;
-        pNODE* A;
+        //pNODE* A;
         //char* theVertices = (char*)malloc(sizeof(int) *vertices);
         //char* a = new char[vertices];
-        A = (pNODE*)calloc(vertices + 1, sizeof(pNODE));
-
-        /* for (int i = 0; i < edges; i++) {
+       // A = (pNODE*)calloc(vertices + 1, sizeof(pNODE));
+        //NODE* pointer = new NODE[vertices];
+        /*for (int i = 0; i < edges; i++) {
             // a[i] = i + 1;
          }*/
-
+        struct Graph* graph = createGraph(vertices);
         for (int i = 0; i < edges; i++) {
             inFile >> edgeID;
             inFile >> vertexu;
             inFile >> vertexv;
             inFile >> weight;
-            addEdge(A, vertexu, vertexv);
-
-            std::cout << edgeID << vertexu << vertexv << weight << std::endl;
+            addEdge(graph, vertexu, vertexv, weight);
         }
-        //delete[] a;
     }
     else {
         std::cout << "Error: cannot open file for reading" << std::endl;
@@ -58,7 +89,6 @@ void command() {
         std::cin >> fileName;
         std::cin >> weight;
         readFromFile(fileName);
-
     }
 }
 
